2cbeb4d93a109683dd5ae13d63c6a478577ca2d2
Darshan-Gowda <darshan.rmgm@gmail.com>
2014-08-22 10:53:41 +0530

    Update config.py

--- python-analysis-code/config.py
+++ python-analysis-code/config.py
@@ -3,6 +3,6 @@ All configuration stuff will go here
 """
 
 CONFIG = {"Analysis_tool1": "pep8",  # use full system path for pep8 in case of windows, if cent-os use default config
-          "Analysis_tool2": "pyflakes",  # use full system path for pep8 in case of windows,
+          "Analysis_tool2": "pyflakes",  # use full system path for pyflakes in case of windows,
                                          # if cent-os use default config
           "pep8_config_file_path": ""}  # pep8 config file full system path if any
