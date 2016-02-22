d609ee93bb93b2a0bfeff4379d618bbd8b16282a
DarshanGowda <rdarshan@zeomega.com>
2016-02-22 12:28:03 +0530

    removed dependency on report_config and added docs

--- scanForSelectInHtml.py
+++ scanForSelectInHtml.py
@@ -10,14 +10,116 @@ output:
 import glob
 import os
 import sys
-import report_config
+
 try:
+    # to be in compatible with jiva buildout python
     from bs4 import BeautifulSoup
 except ImportError:
     from BeautifulSoup import BeautifulSoup
 
 
+class ReportGenerator():
+    """
+    ReportGenerator class can be used to generate basic report with its class attributes
+    """
+    def __init__(self):
+        self.report_header = """<html>
+        <head>
+            <title>Select Tag Report</title>
+            <style type="text/css">body {
+                font-family: Arial, sans-serif;
+                margin: 20px 20px 20px 30px;
+                }
+
+                .repo {
+                font-weight: bold;
+                }
+
+                .tableHeader {
+                font-weight: bold;
+                }
+
+                .file {
+                text-align: left;
+                }
+
+                .num {
+                text-align: center;
+                }
+
+                .pattern {
+                font-weight: bold;
+                text-align: center;
+                color: #990000;
+                background-color: #FFAAAA;
+                }
+
+                table {
+                border: 2px solid gray;
+                border-collapse: collapse;
+
+                -moz-box-shadow: 3px 3px 4px #AAA;
+                -webkit-box-shadow: 3px 3px 4px #AAA;
+                box-shadow: 3px 3px 4px #AAA;
+                }
+
+                td, th {
+                border: 1px solid #D3D3D3;
+                padding: 4px 15px 4px 15px;
+                margin: 20px 15px 20px 15px;
+                }
+
+                th {
+                text-shadow: 2px 2px 2px white;
+                }
+
+                th {
+                border-bottom: 1px solid gray;
+                background-color: #DDDDFF;
+                }
+            </style>
+        </head>
+        <body>
+        <body>
+        <div>
+            <h2>Select Tag Scan Report</h2>
+            <table>
+                <tbody>
+                <tr class="tableHeader">
+                    <th>Module</th>
+                    <th>File Name</th>
+                    <th>Count Of Select Identified</th>
+                    <th>Patterns Found</th>
+                </tr>
+        """
+
+        self.report_footer = """</tbody>
+            </table>
+        </div>
+        </body>
+        </html>"""
+
+        self.report_repo_heading_header = """
+        <tr>
+                    <td class="repo">{0}</td>
+                    <td class="file">{1}</td>
+                    <td class="num">{2}</td>
+                    <td class="pattern">{3}</td>
+                </tr>
+        """
+
+        self.report_repo_value_header = """
+        <tr>
+                    <td></td>
+                    <td class="file">{1}</td>
+                    <td class="num">{2}</td>
+                    <td class="pattern">{3}</td>
+                </tr>
+        """
+
+
 def check_any_dir_exist(html_occurence):
+    """checks if a path is a dir, then expands it to files level(only one level)"""
     html_container = []
     for each_path in html_occurence:
         if os.path.isdir(each_path):
@@ -28,8 +130,10 @@ def check_any_dir_exist(html_occurence):
 
 
 def get_files_with_select_tag():
+    """scans for htmls under components and generates report"""
     components = sys.argv[1:] if sys.argv[1:] else glob.glob('*')
     report_body = ""
+    report_gen = ReportGenerator()
     for each_repo in components:
         component_flag = True
         try:
@@ -40,22 +144,22 @@ def get_files_with_select_tag():
                 if select_group:
                     select_group = ['<p>'+str(each_select)+'</p>' for each_select in select_group]
                     if component_flag:
-                        report_body += report_config.report_repo_heading_header.format(each_repo, each_html,
-                                                                                       len(select_group),
-                                                                                       ''.join(select_group).replace(
-                                                                                           '<select', 'select'))
+                        report_body += report_gen.report_repo_heading_header.format(each_repo, each_html,
+                                                                                    len(select_group),
+                                                                                    ''.join(select_group).replace(
+                                                                                        '<select', 'select'))
                     else:
-                        report_body += report_config.report_repo_value_header.format(each_repo, each_html,
-                                                                                     len(select_group),
-                                                                                     ''.join(select_group).replace(
-                                                                                         '<select', 'select'))
+                        report_body += report_gen.report_repo_value_header.format(each_repo, each_html,
+                                                                                  len(select_group),
+                                                                                  ''.join(select_group).replace(
+                                                                                      '<select', 'select'))
                     component_flag = False
 
         except Exception as e:
             print e, each_repo
 
     with open("report.html", "w") as f:
-        f.write(report_config.report_header + report_body + report_config.report_footer)
+        f.write(report_gen.report_header + report_body + report_gen.report_footer)
 
 if __name__ == "__main__":
     get_files_with_select_tag()
