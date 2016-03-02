672cdb9f9ffdce4b22970de79c0aedd1ad3c2e73
DarshanGowda <rdarshan@zeomega.com>
2016-03-02 13:13:51 +0530

    optimized script with bs4 adv features

--- zesearch.py
+++ zesearch.py
@@ -1,26 +1,31 @@
 __author__ = 'Darshan RK Gowda'
 
 import requests
-import re
 from bs4 import BeautifulSoup
 
-company_contacts = {}
 response = requests.get('https://x.y.z/search.php?q=', verify=False)
-raw_html = re.findall(r'<table.*>.*</table>', response.text)[0]
-reformatted_html = re.sub(r'<img.*?>', '', raw_html)
-soup = BeautifulSoup(reformatted_html, 'html.parser')
-tr_child = soup.find_all("tr")
+soup = BeautifulSoup(response.text, 'lxml')  # use lxml to scan even broken html
+basic_info = {}
 
-f = open('contacts.csv', 'w+')
-f.write("Full Name, Email, Extension\n")
+for each_div in soup.findAll('div', onclick=True):
+    basic_info_tag = each_div.findPrevious(name='tr')
+    basic_info_tag_childs = basic_info_tag.find_all('td')
 
-for index in range(1, len(tr_child), 10):
-    td_childs = tr_child[index].find_all('td')
-    try:
-        f.write(str(td_childs[0].contents[0]).replace(",", "#") + "," + str(td_childs[1].contents[0]) + "," + str(
-            td_childs[2].contents[0]) + "\n")
-    except Exception as e:
-        print e
+    emp_name = basic_info_tag_childs[1].text  # scaning basic info
+    if emp_name:
+        basic_info[emp_name] = {"name": basic_info_tag_childs[0].text,
+                                "extn": basic_info_tag_childs[2].text}
 
-f.close()
+        reportees_tags = basic_info_tag.next_sibling.findAll(class_="myclass")  # scanning more info
+        reporter_name = reportees_tags[0].attrs['rel']
+        basic_info[emp_name]['reportees'] = "#".join(reportees_tags[1].text.split(','))
+        basic_info[emp_name]['reportingTo'] = reporter_name
 
+with open('contacts.csv', 'w') as fd:
+    fd.write("Full Name, Email, Extension, reportees, reportingTo\n")
+    for email, info in basic_info.items():
+        try:
+            fd.write(",".join([info['name'], email, info['extn'], info['reportees'], info['reportingTo']]).encode(
+                'utf-8') + "\n")
+        except Exception as e:
+            print e
